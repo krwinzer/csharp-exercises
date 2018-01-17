87be6687da31b8fdc6a6e43f8e0db77001926092
Kelly Winzer <kelly.r.winzer@gmail.com>
2018-01-16 20:18:27 -0600

    Updates to AreaCircle done in class.

--- AreaCircle/Program.cs
+++ AreaCircle/Program.cs
@@ -6,26 +6,23 @@ namespace AreaCircle
     {
         static void Main(string[] args)
         {
-            double area;
-            double radius;
-            double pi;
-            string input;
-
-            Console.WriteLine("Enter a radius: ");
-            input = Console.ReadLine();
-            radius = double.Parse(input);
-            pi = 3.14;
-
-            while (radius < 0)
+            while (true)
             {
-                Console.WriteLine("Please do not use a negative number. Enter a new radius:");
-                input = Console.ReadLine();
-                radius = double.Parse(input);
-            }
+                Console.WriteLine("Enter a radius: ");
+                string input = Console.ReadLine();
+                double radius = double.Parse(input);
 
-            area = pi * radius * radius;
-            Console.WriteLine("The area of a circle where radius is " + radius + " is " + area);
-            Console.ReadLine();
+                while (radius < 0)
+                {
+                    Console.WriteLine("Please do not use a negative number. Enter a new radius:");
+                    input = Console.ReadLine();
+                    radius = double.Parse(input);
+                }
+
+                double area = Math.PI * radius * radius;
+                Console.WriteLine(string.Format("The area of a circle where radius is {0} is {1:.00}", radius, area));
+                Console.ReadLine();
+            }
         }
     }
 }
